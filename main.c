c71be7f9295634242d1e3e5519d9b7e0d88bc0df
Ivan Frolov <frolik@Ivans-MacBook-Pro.local>
2022-11-30 18:59:35 -0500

    fix: typos

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 
 #include <stdio.h>
 
+// Including function prototypes
 #include "converting.h"
 #include "fundamentals.h"
 #include "manipulating.h"
 #include "tokenizing.h"
+
 int main(void)
 {
+    // declaring cstring variable with length of 10
     char buff[10];
     do {
         printf("1 - Fundamentals\n");
@@ -16,7 +19,15 @@ int main(void)
         printf("4 - Tokenizing\n");
         printf("0 - Exit\n");
         printf("Which module to run? \n");
+
+        // Read string from stdin, stops when 9 characters are read or \n is reached
+        // and stores to buffer1
         fgets(buff, 10, stdin);
+
+        // buff first character == '1' -> call fundamentals function
+        // buff first character == '2' -> call manipulating function
+        // buff first character == '3' -> call converting function
+        // buff first character == '4' -> call tokenizing function
         switch (buff[0]) {
             case '1':
                 fundamentals();
@@ -31,6 +42,8 @@ int main(void)
                 tokenizing();
                 break;
         }
+        // while (first character of buff != '0')
     } while (buff[0] != '0');
+
     return 0;
 }
